b4e48de15d12aa3729d7ea8f6d37e86147e9c5bd
Putrimaulida <2041720012@student.polinema.ac.id>
2022-04-26 00:01:18 +0700

    Praktikum 2: CRUD dalam RESTful API

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
 });
+
